2588b2e03d726be9869099847541439058f36bd8
David Martin <david.martin@gmail.com>
2024-04-20 12:47:30 -0500

    init

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,14 +1,55 @@
-import React from "react";
-
+import React, { useContext } from 'react';
+import { useTreatments, SplitContext } from '@splitsoftware/splitio-react';
 import { APP_NAME } from "../../lib/utils/constant";
 
-const Banner = () => (
-  <div className="banner">
-    <div className="container">
-      <h1 className="logo-font">{APP_NAME.toLowerCase()}</h1>
-      <p>A place to share your knowledge.</p>
-    </div>
-  </div>
-);
+function renderContent(treatmentWithConfig) {
+  const { treatment, config } = treatmentWithConfig;
+  console.log('Banner.renderContent.treatment: ' + treatment);
+  
+  const json = JSON.parse(config || '');
+  const text = json.text;
+  return (
+    <>
+      <div className="banner">
+        <div className="container">
+          <h1 className="logo-font">{APP_NAME.toLowerCase()}</h1>
+          <p>{text || "A place to share your wackiness."}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+const Banner = () => {
+  // Obtain readiness and other context properties as needed
+  const { isReady, client } = useContext(SplitContext);
+
+  // Define the feature flag name you're interested in
+  const splitName = 'next_banner';
+
+  // Optionally, you can pass attributes if your treatment logic depends on them
+  const attributes = {
+    // Attributes like user id or other targeting properties
+  };
+
+  // Fetch treatments from Split.io
+  const treatments = useTreatments([splitName], attributes);
+
+  let result;
+  if(isReady) {
+    result = renderContent(treatments[splitName]);
+  } else {
+    result = (    
+      <>
+      <div className="banner">
+        <div className="container">
+          <h1 className="logo-font">{APP_NAME.toLowerCase()}</h1>
+          <p>{"loading..."}</p>
+        </div>
+      </div>
+    </>);
+  } 
+  return result;
+};
 
 export default Banner;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,23 @@ import Layout from "components/common/Layout";
 import ContextProvider from "lib/context";
 import "styles.css";
 
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+const uuid = uuidv4();
+
+import { SplitFactoryProvider } from '@splitsoftware/splitio-react';
+const sdkConfig: SplitIO.IBrowserSettings = {
+  core: {
+    authorizationKey: 'kbef92s0e0gum4h7o7hqqeuc6uvbdmjmvfaj',
+    key: uuidv4()
+  }
+};
+
 if (typeof window !== "undefined") {
   require("lazysizes/plugins/attrchange/ls.attrchange.js");
   require("lazysizes/plugins/respimg/ls.respimg.js");
@@ -19,11 +36,13 @@ const MyApp = ({ Component, pageProps }) => (
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
       />
     </Head>
-    <ContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ContextProvider>
+    <SplitFactoryProvider config={sdkConfig} >    
+      <ContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ContextProvider>
+    </SplitFactoryProvider>  
   </>
 );
 
